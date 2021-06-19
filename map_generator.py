fb025b25cafecee28d25a61c18d6f5aa4714339d
Alon <alonbeilis@gmail.com>
2021-06-19 16:29:22 +0300

    script is working, including the movment of the robot towords center of mass

--- map_generator.py
+++ map_generator.py
@@ -1,8 +1,8 @@
-import numpy as np
-from scipy import ndimage as nd
 import math
+import numpy as np
 import random
 
+from scipy import ndimage as nd
 from typing import Tuple
 
 Point = Tuple[int, int]
@@ -10,12 +10,13 @@ Point = Tuple[int, int]
 
 class Map:
     def __init__(self, agent_location: Point = None, alpha: float = 0.3, init_prob_value: float = 0.5,
-                 p_ta: float = 1.0, size: int = 20, sensor_info: int = 10, targets_locations=None):
+                 p_ta: float = 1.0, probability_to_match: float = 0.95, size: int = 20, sensor_info: int = 10, targets_locations=None):
         self.agent_point = agent_location
         self.board_size = size  # assuming board is symmetrical
         self.p_fa = alpha * p_ta
         self.p_ta = p_ta
         self.probability_map = np.full((size, size), init_prob_value)
+        self.probability_to_match = probability_to_match
         self.sensor_info = sensor_info
 
         if agent_location is None:
@@ -28,32 +29,61 @@ class Map:
     @staticmethod
     def _calc_distance_between_points(point_a: Point, point_b: Point) -> float:
         """
-        Calculating Euclidean distance between two points
+        Calculating Euclidean distance between two points.
+        :returns a float, indicating the distance between two points.
         """
         return math.hypot(point_a[0] - point_b[0], point_a[1] - point_b[1])
 
     def _iterate_map(self) -> Tuple[Tuple[int, int], np.float64]:
+        """
+        Iterating over the map.
+        :returns tuple of the point index, value of map[index].
+        """
         for idx, value in np.ndenumerate(self.probability_map):
             yield idx, value
 
     def _is_agent(self, point: Point) -> bool:
+        """
+        Checking if a point is the agent point.
+        :returns a boolean.
+        """
         return point == self.agent_point
 
     def _is_target(self, point: Point) -> bool:
+        """
+        Checking if a point is a target point.
+        :returns a boolean.
+        """
         return point in self.targets
 
     def _get_probability_for_point(self, point: Point) -> float:
+        """
+        Calculating the probability for getting a signal in a certain point.
+        :returns float, indicating the probability.
+        """
         return self.p_ta if self._is_target(point) else self.p_fa
 
     def _calc_weight_factor_ci(self, ci: Point) -> float:
+        """
+        Calculating the weight factor for a point
+        :returns a float, indicating the weight factor.
+        """
         distance = Map._calc_distance_between_points(point_a=self.agent_point, point_b=ci)
         return 1 / math.exp(distance / self.sensor_info)
 
     def calc_x_ci(self, point: Point, weight_factor: float) -> bool:
+        """
+        Calculating whether a point got a signal or not.
+        :returns boolean.
+        """
         probability = self._get_probability_for_point(point=point)
         return True if (random.random() < (probability * weight_factor)) else False
 
-    def _calc_p_s_ci(self, idx: Point, ci_value: float, weight_factor_ci: float):
+    def _calc_p_s_ci(self, idx: Point, ci_value: float, weight_factor_ci: float) -> float:
+        """
+        Calculating the probability for finding a target, per point.
+        :returns a float, indicating the probability.
+        """
         x_ci = self.calc_x_ci(point=idx, weight_factor=weight_factor_ci)
         p_s_ci_prev = ci_value
 
@@ -66,24 +96,32 @@ class Map:
 
         return num / denum
 
-    def get_s_single_frame(self):
+    def update_probability_map_single_frame(self):
+        """
+        Iterates over the map.
+        for each point, calculates the probability for finding a target.
+        """
         for idx, ci_value in self._iterate_map():
             weight_factor_ci = self._calc_weight_factor_ci(ci=idx)
-
             self.probability_map[idx] = self._calc_p_s_ci(idx=idx, ci_value=ci_value, weight_factor_ci=weight_factor_ci)
 
-        return self.probability_map
-
     def calc_center_of_mass(self) -> Point:
+        """
+        For the probability map, calculates the center of mass of probabilities.
+        :returns a tuple, indicating center of mass coordinates.
+        """
         center_of_mass = nd.center_of_mass(self.probability_map)
         return tuple(np.round(center_of_mass))
 
-    def move_agent_single_step(self, probability_center_of_mass: Point):
-        if self.agent_point == probability_center_of_mass:
+    def move_agent_single_step(self, point: Point):
+        """
+        Moves the agent one step towards point.
+        """
+        if self.agent_point == point:
             return
 
         agent_pos_x, agent_pos_y = self.agent_point
-        com_pos_x, com_pos_y = probability_center_of_mass
+        com_pos_x, com_pos_y = point
 
         x_diff = com_pos_x - agent_pos_x
         y_diff = com_pos_y - agent_pos_y
@@ -93,15 +131,18 @@ class Map:
 
         self.agent_point = int(agent_pos_x), int(agent_pos_y)
 
-    def targets_found(self):
-        return (self.probability_map[tuple(zip(*self.targets))] > 0.95).all()
+    def all_targets_found(self) -> bool:
+        """
+        Checks if all of the targets found - probability is over the probability to match
+        :returns boolean
+        """
+        return bool((self.probability_map[tuple(zip(*self.targets))] > self.probability_to_match).all())
 
     def run_algo(self, max_time_to_run: int):
         for t in range(max_time_to_run):
-            self.probability_map = self.get_s_single_frame()
-            probability_center_of_mass = self.calc_center_of_mass()
-            self.move_agent_single_step(probability_center_of_mass)
-            if self.targets_found():
+            self.update_probability_map_single_frame()
+            self.move_agent_single_step(point=self.calc_center_of_mass())
+            if self.all_targets_found():
                 return t
 
 
@@ -110,9 +151,10 @@ if __name__ == '__main__':
                    alpha=0.5,
                    init_prob_value=0.5,
                    p_ta=1.0,
+                   probability_to_match=0.95,
                    size=20,
                    sensor_info=10,
-                   targets_locations=[(1, 0), (1,6), (8,4)])
+                   targets_locations=[(1, 0), (1, 6), (8, 4)])
 
     time_to_find_targets = main_map.run_algo(max_time_to_run=10000)
     print(f'It took {time_to_find_targets} iterations to find the targets')
